3fbafb73e8aa353c91c04724551db21af87d80a3
zhisheng <zhishengtrieu@gmail.com>
2023-03-27 10:05:01 -0400

    Déplacement du code #4

--- main.py
+++ main.py
@@ -24,11 +24,21 @@ running = True
 home = True
 maze = None
 display_solution = False
+end_game = False
 
 accueil = Home(screen, window_width, window_height, font)
 
+# on charge le bouton pour sortir du jeu ou revenir au menu
+quit_button = pygame.image.load('assets/exit.png')
+quit_button = pygame.transform.scale(quit_button, (60, 60))
+quit_button_rect = quit_button.get_rect()
+quit_button_rect.x = window_width - quit_button_rect.width - 10
+quit_button_rect.y = 10
+
 while running:
     if home:
+        if end_game:
+            end_game = False
         accueil.actualiser()
 
         # on met un contrôleur d'événement pour les boutons
@@ -49,49 +59,28 @@ while running:
                     taille_case = window_height // max(accueil.longueur, accueil.hauteur)
                     maze = Maze(accueil.longueur, accueil.hauteur, taille_case)
                     player = maze.player
+                # on ajoute le contrôleur d'événements pour le bouton
+                if quit_button_rect.collidepoint(pygame.mouse.get_pos()):
+                    if home:
+                        running = False
+                        home = False
             if event.type == pygame.QUIT:
                 running = False
                 home = False
     else:
-        screen.fill((0, 0, 0))
-        graphe = maze.graphe
-        for noeud in graphe.get_noeuds():
-            # on dessine un cercle pour chaque nœud
-            pygame.draw.circle(screen, (255, 255, 255),
-                               ((noeud.get_x() + 1 / 2) * taille_case, (noeud.get_y() + 1 / 2) * taille_case),
-                               taille_case * 0.9 // 2)
-            # on dessine un arc entre chaque nœud et ses voisins
-            for arc in noeud.get_arcs():
-                pygame.draw.line(screen, (255, 255, 255),
-                                 ((noeud.get_x() + 1 / 2) * taille_case, (noeud.get_y() + 1 / 2) * taille_case),
-                                 ((arc.get_destination().get_x() + 1 / 2) * taille_case,
-                                  (arc.get_destination().get_y() + 1 / 2) * taille_case),
-                                 int(taille_case * 0.9))
+        maze.actualiser(screen, taille_case, display_solution, accueil.hard_mod)
 
-        if display_solution:
-            # on affiche la solution
-            for i in range(len(maze.solution) - 1):
-                pygame.draw.line(screen, (255, 0, 0),
-                                 ((maze.solution[i].get_x() + 1 / 2) * taille_case,
-                                  (maze.solution[i].get_y() + 1 / 2) * taille_case),
-                                 ((maze.solution[i + 1].get_x() + 1 / 2) * taille_case,
-                                  (maze.solution[i + 1].get_y() + 1 / 2) * taille_case),
-                                 taille_case // 2)
-                # on dessine un cercle sur chaque nœud de la solution
-                pygame.draw.circle(screen, (255, 0, 0),
-                                   ((maze.solution[i].get_x() + 1 / 2) * taille_case,
-                                    (maze.solution[i].get_y() + 1 / 2) * taille_case),
-                                   taille_case // 4)
+        # on affiche le message de victoire si le joueur arrive à la fin
+        if player.position == maze.fin:
+            home = True
+            display_solution = False
+            maze = None
+            end_game = True
 
-        # on affiche le depart et l'arrivée
-        img_depart = pygame.image.load('assets/kamehouse.png')
-        img_depart = pygame.transform.scale(img_depart, (taille_case // 2, taille_case // 2))
-        screen.blit(img_depart, (maze.depart.get_x() * taille_case + img_depart.get_width() // 2,
-                                 maze.depart.get_y() * taille_case + img_depart.get_height() // 2))
-
-        img_fin = pygame.image.load('assets/arrivee.png')
-        img_fin = pygame.transform.scale(img_fin, (taille_case / 2, taille_case / 2))
-        screen.blit(img_fin, ((maze.fin.get_x() + 1 / 4) * taille_case, (maze.fin.get_y() + 1 / 4) * taille_case))
+            msg = font.render('You Win !', True, (255, 0, 0))
+            # on veut que le message apparaisse au milieu de l'écran
+            msg_rect = msg.get_rect(center=(window_width // 2, window_height // 2))
+            screen.blit(msg, msg_rect)
 
         for event in pygame.event.get():
             match event.type:
@@ -112,43 +101,22 @@ while running:
                         case pygame.K_ESCAPE:
                             maze.resolution()
                             display_solution = True
-
-        # on affiche le joueur
-        player.draw(screen)
-        maze.zoro.draw(screen)
-
-        # on affiche l'ombre si le mode difficile est activé
-        if accueil.hard_mod:
-            player.draw_ombre(screen)
-
-        # on affiche le message de victoire si le joueur arrive à la fin
-        if player.position == maze.fin:
-            home = True
-            display_solution = False
-            maze = None
-
-            msg = font.render('You Win !', True, (255, 0, 0))
-            # on veut que le message apparaisse au milieu de l'écran
-            msg_rect = msg.get_rect(center=(window_width // 2, window_height // 2))
-            screen.blit(msg, msg_rect)
+                case pygame.MOUSEBUTTONDOWN:
+                    # on ajoute le contrôleur d'événements pour le bouton pour revenir à l'accueil
+                    if quit_button_rect.collidepoint(pygame.mouse.get_pos()):
+                        if not home:
+                            home = True
+                            display_solution = False
+                            maze = None
+                            break
 
         # on met un délai pour éviter que le jeu retourne instantanément à l'accueil
-        if home:
+        if home and end_game:
             pygame.display.update()
             pygame.time.wait(800)
 
-    # on charge le bouton pour sortir du jeu
-    quit_button = pygame.image.load('assets/exit.png')
-    quit_button = pygame.transform.scale(quit_button, (60, 60))
-    quit_button_rect = quit_button.get_rect()
-    quit_button_rect.x = window_width - quit_button_rect.width - 10
-    quit_button_rect.y = 10
     screen.blit(quit_button, quit_button_rect)
-    # on ajoute le contrôleur d'événements pour le bouton
-    if quit_button_rect.collidepoint(pygame.mouse.get_pos()):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            running = False
-            home = False
+
     pygame.display.update()
 
 pygame.quit()
--- src/Maze.py
+++ src/Maze.py
@@ -23,7 +23,6 @@ class Maze:
         self.algo = DFS(self.graphe)
         self.algo.generate()
         self.player = Player(self.depart, self.taille_case)
-        self.resolution()
         pos = random.choice(self.graphe.noeuds)
         self.zoro = Zoro(pos, self.taille_case)
 
@@ -32,3 +31,62 @@ class Maze:
         On résout le labyrinthe
         """
         self.solution = self.algo.find_shortest_path(self.player)
+
+    def draw_solution(self, screen):
+        """
+        On dessine la solution
+        """
+        self.resolution()
+        for i in range(len(self.solution) - 1):
+            pygame.draw.line(screen, (255, 0, 0),
+                             ((self.solution[i].get_x() + 1 / 2) * self.taille_case,
+                              (self.solution[i].get_y() + 1 / 2) * self.taille_case),
+                             ((self.solution[i + 1].get_x() + 1 / 2) * self.taille_case,
+                              (self.solution[i + 1].get_y() + 1 / 2) * self.taille_case),
+                             self.taille_case // 2)
+            # on dessine un cercle sur chaque nœud de la solution
+            pygame.draw.circle(screen, (255, 0, 0),
+                               ((self.solution[i].get_x() + 1 / 2) * self.taille_case,
+                                (self.solution[i].get_y() + 1 / 2) * self.taille_case),
+                               self.taille_case // 4)
+
+    def actualiser(self, screen, taille_case, display_solution, hard_mod):
+        """
+        On actualise le labyrinthe
+        """
+        screen.fill((0, 0, 0))
+        graphe = self.graphe
+        for noeud in graphe.get_noeuds():
+            # on dessine un cercle pour chaque nœud
+            pygame.draw.circle(screen, (255, 255, 255),
+                               ((noeud.get_x() + 1 / 2) * taille_case, (noeud.get_y() + 1 / 2) * taille_case),
+                               taille_case * 0.9 // 2)
+            # on dessine un arc entre chaque nœud et ses voisins
+            for arc in noeud.get_arcs():
+                pygame.draw.line(screen, (255, 255, 255),
+                                 ((noeud.get_x() + 1 / 2) * taille_case, (noeud.get_y() + 1 / 2) * taille_case),
+                                 ((arc.get_destination().get_x() + 1 / 2) * taille_case,
+                                  (arc.get_destination().get_y() + 1 / 2) * taille_case),
+                                 int(taille_case * 0.9))
+
+        if display_solution or self.player.position == self.zoro.position:
+            # on affiche la solution
+            self.draw_solution(screen)
+
+        # on affiche le depart et l'arrivée
+        img_depart = pygame.image.load('assets/kamehouse.png')
+        img_depart = pygame.transform.scale(img_depart, (taille_case // 2, taille_case // 2))
+        screen.blit(img_depart, (self.depart.get_x() * taille_case + img_depart.get_width() // 2,
+                                 self.depart.get_y() * taille_case + img_depart.get_height() // 2))
+
+        img_fin = pygame.image.load('assets/arrivee.png')
+        img_fin = pygame.transform.scale(img_fin, (taille_case / 2, taille_case / 2))
+        screen.blit(img_fin, ((self.fin.get_x() + 1 / 4) * taille_case, (self.fin.get_y() + 1 / 4) * taille_case))
+
+        # on affiche le joueur
+        self.player.draw(screen)
+        self.zoro.draw(screen)
+
+        # on affiche l'ombre si le mode difficile est activé
+        if hard_mod:
+            self.player.draw_ombre(screen)
